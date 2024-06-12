98cf18b2b6d9114548ce92c9ee43029a23c59798
Raj Rasane <rasaneraj04@gmail.com>
2024-06-12 19:16:49 +0530

    new notes added

---
+++ Python Notes/03]_Operators.py
@@ -0,0 +1,33 @@
+# Operators
+
+# 1) Arithmetic Operators
+
+x = 10
+y = 5
+
+print(x + y)  # Addition
+print(x - y)  # Subtraction
+print(x * y)  # Multiplication
+print(x / y)  # Division
+print(x % y)  # Modulus
+print(x ** y) # Exponentiation
+print(x // y) # Floor Division
+
+# ---------------------------------------------------------------------------------------------------------------------------------------
+
+# 2)  Comparison Operators
+
+print(x == y)  # Equal to
+print(x != y)  # Not equal to
+print(x > y)   # Greater than
+print(x < y)   # Less than
+print(x >= y)  # Greater than or equal to
+print(x <= y)  # Less than or equal to
+
+# ---------------------------------------------------------------------------------------------------------------------------------------
+
+# 3) Logical Operators
+
+print(x > 5 and y < 10)  # and
+print(x > 5 or y < 5)    # or
+print(not(x > 5 and y < 10))  # not
