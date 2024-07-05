c1c1e02c46c8e78041dc088db2d263d1db01b916
Raj Rasane <rasaneraj04@gmail.com>
2024-07-05 20:01:01 +0530

    new files added

--- Python Notes/00]_helloworld.py
+++
@@ -1,3 +0,0 @@
-# This is how you print anything on console or display using print function in python
-
-print("Hello World")
---
+++ Python Notes/Practice.py
@@ -0,0 +1,5 @@
+x = 10
+y = 20
+
+# Use f-strings (formatted string literals) for a cleaner approach
+print(f"X has value: {x} and Y has value: {y}")
