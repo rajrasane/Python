91bc17fc324d489e43e8a30c7e4c4ab9d9710b34
Raj Rasane <rasaneraj04@gmail.com>
2024-08-01 21:19:53 +0530

    Advance python topics added (OOPs)

---
+++ advance_python/01]_class.py
@@ -0,0 +1,22 @@
+# Class
+
+# - A class is considered a blueprint of objects
+
+#---------------------------------------------------------------------------------------------------------------------------------------
+
+# Creating a class
+
+# - A class can be created using the keyword 'class' followed by the ClassName
+
+# syntax :-
+
+# class Classname:
+    # class definition
+
+#---------------------------------------------------------------------------------------------------------------------------------------
+
+# for e.g
+
+class Student:          # here the ClassName is 'Student'
+    name = "Raj"
+    roll_no = 10
