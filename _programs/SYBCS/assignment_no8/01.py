3033e275e5664a4d5b90cb865d324d7c1feeeb42
Raj Rasane <rasaneraj04@gmail.com>
2024-08-31 20:38:03 +0530

    added a circle

--- _programs/SYBCS/assignment_no8/01.py
+++ _programs/SYBCS/assignment_no8/01.py
@@ -4,8 +4,6 @@ screen = turtle.Screen()
 
 t = turtle.Turtle()
 
-t.speed(1)
-
 t.forward(100)
 
 t.left(90)
@@ -13,5 +11,9 @@ t.left(90)
 t.forward(100)
 
 t.left(90)
+
+t.home()
+
+t.circle(50)
 
 turtle.done()
