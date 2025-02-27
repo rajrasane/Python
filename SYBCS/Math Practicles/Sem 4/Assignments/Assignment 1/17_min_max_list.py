844a58d1be9f96708b27b4224b7b11d477549f55
Raj Rasane <rasaneraj04@gmail.com>
2025-02-28 00:21:19 +0530

    corrected question

--- SYBCS/Math Practicles/Sem 4/Assignments/Assignment 1/17_min_max_list.py
+++ SYBCS/Math Practicles/Sem 4/Assignments/Assignment 1/17_min_max_list.py
@@ -1,4 +1,4 @@
-#Write Python code to find maximum and minimum element in the given list. [7,8,71, 32, 49, -5,7,7,0,1,6)
+#Write Python code to find maximum and minimum element in the given list. [7,8,71, 32, 49, -5,7,7,0,1,6]
 list1 = [7,8,71, 32, 49, -5,7,7,0,1,6]
 print("Maximum element in list:-",max(list1))
 print("Minimum element in list:-",min(list1))
