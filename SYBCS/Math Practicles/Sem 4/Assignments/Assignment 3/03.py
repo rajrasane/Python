8fa5aa21369810e9b045c6e6477f59941ef129c3
Raj Rasane <rasaneraj04@gmail.com>
2025-02-27 19:37:50 +0530

    Moved files to accurate folder

--- SYBCS/Math Practicles/Sem 4/Assignments/Assignment 3/03.py
+++ SYBCS/Math Practicles/Sem 4/Assignments/Assignment 3/03.py
@@ -1,25 +1,7 @@
-import math
+# Write a program to check if a year is a leap year.
 
-def f(x):
-    return (x * math.sin(x)) * math.cos(x)
+def chechyear(year):
+    if(year%4==0):
+        print("Year is a leap year")
 
-def falsi(x0, x1, n, f):
-    for i in range(n):
-        x2 = x0 - (f(x0) * (x1 - x0)) / (f(x1) - f(x0))
-
-        print(f"Iteration {i+1}: x2 = {x2}")
-
-        if abs(f(x2)) < 1e-6:
-            print(f"Root found at x = {x2}")
-            return x2
-
-        if f(x0) * f(x2) < 0:
-            x1 = x2 
-        else:
-            x0 = x2  
-    
-    print("Max iterations reached")
-    return x2  
-
-root = falsi(1, 3, 4, f)
-print(f"Final approximation of the root: {root}")
+chechyear(2023)
