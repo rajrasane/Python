b6f76cccf1164a9c01ed1154e2861880cf744f87
Raj Rasane <rasaneraj04@gmail.com>
2025-03-18 20:26:23 +0530

    changed the execution

--- SYBCS/Math Practicles/Sem 4/All Concepts/newton-raphson.py
+++ SYBCS/Math Practicles/Sem 4/All Concepts/newton-raphson.py
@@ -1,17 +1,34 @@
-def newton_raphson(f, g, x0, e, N):
-    for step in range(1, N+1):
-        if g(x0) == 0:
-            print("Divide by zero error!")
-            return
+def Newton_Raphson(f, g, x0, e, N): 
+    x0 = float(x0) 
+    e = float(e) 
+    N = int(N) 
+    step = 1 
+    flag = 1 
+    condition = True
+    while condition:
+        if g(x0) == 0.0:
+            print('Divide by zero error!')
+            break
         x1 = x0 - f(x0) / g(x0)
-        print(f"Iteration-{step}, x1={x1:.6f}, f(x1)={f(x1):.6f}")
-        if abs(f(x1)) <= e:
-            print(f"\nRoot: {x1:.8f}")
-            return
+        print('Iteration-%d, x1=%0.6f and f(x1)=%0.6f' % (step, x1, f(x1)))
         x0 = x1
-    print("\nNot convergent.")
+        step += 1
+        
+        if step > N:
+            flag = 0
+            break
+        
+        condition = abs(f(x1)) > e
 
-def f(x): return x**3 - 5*x + 1
-def g(x): return 3*x**2 - 5
-
-newton_raphson(f, g, 0.1, 1e-5, 100)
+    if flag == 1:
+        print('\nRequired root is: %0.8f' % x1)
+    else:
+        print('\nNot convergent.')
+    
+def f(x): return x ** 3 - 5 * x + 1
+def g(x): return 3 * x ** 2 - 5
+x0 = 0.1 
+e = 0.00001 
+N = 100
+    
+Newton_Raphson(f, g, x0, e, N)
